193998dc4db6bfa81f74bd0fab8ff36660568e4b
Daria Puch <dar-puch@users.noreply.github.com>
2018-08-04 01:19:45 +0200

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 import BookShelf from './BookShelf';
-import * as BooksAPI from './BooksAPI'
-
+import * as BooksAPI from './BooksAPI';
+import {Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import Search from './search';
 class BooksApp extends Component {
   state = {
-    showSearchPage: false,
     books: [],
   }
 move = this.move.bind(this);
@@ -34,30 +35,8 @@ this.setState({books: books})}).then(console.log('state set'))
     return (
 
       <div className="app">
-
-
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+      <Route path="/search" component={Search} />
+      <Route exact path = "/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -67,11 +46,12 @@ this.setState({books: books})}).then(console.log('state set'))
 
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to='/search'>Add a book</Link>
             </div>
           </div>
-        )}
-      </div>
+
+     )}/>
+       </div>
     )
   }
 }
